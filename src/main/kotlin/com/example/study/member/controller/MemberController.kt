62dc62758636001782600eb8a576e2b513f432dc
Yookeun <knoc720@gmail.com>
2023-11-17 16:34:29 +0900

    feat: Update member

--- src/main/kotlin/com/example/study/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/study/member/controller/MemberController.kt
@@ -2,13 +2,17 @@ package com.example.study.member.controller
 
 import com.example.study.member.dto.MemberDto
 import com.example.study.member.dto.MemberSearchCondition
+import com.example.study.member.dto.MemberUpdateDto
 import com.example.study.member.service.MemberService
+import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -26,5 +30,12 @@ class MemberController(private val memberService: MemberService) {
     @PreAuthorize("hasAuthority('ADMIN')")
     fun getMember(@PathVariable("id") id: Long): ResponseEntity<MemberDto> {
         return ResponseEntity.ok(memberService.getMember(id))
+    }
+
+    @PatchMapping("/{id}")
+    fun updateMember(@PathVariable("id") id: Long,
+                     @Valid @RequestBody memberUpdateDto: MemberUpdateDto
+                     ): ResponseEntity<MemberDto> {
+        return ResponseEntity.ok(memberService.updateMember(id, memberUpdateDto))
     }
 }
